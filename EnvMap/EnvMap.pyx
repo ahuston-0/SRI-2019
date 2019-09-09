61c284fb8ec29dae6d9c8c30c9768dcb24008da6
ahuston-0 <shuston@stevens.edu>
2019-09-09 17:26:47 -0400

    Fixed transform issue on expansion. Consider looking into single frame glitch during expansion of map.

--- EnvMap/EnvMap.pyx
+++ EnvMap/EnvMap.pyx
@@ -49,7 +49,8 @@ class EnvMap:
         """
         return self._buoy_data
 
-    def set_buoys(self, buoy_list):
+    def set_buoys(self, buoy_list,odom_coords):
+        self._offset = odom_coords
         self._buoy_data = buoy_list
         for item in buoy_list:
             self._curr_map[item[0]+self._offset[0]][item[1]+self._offset[1]] = 30 if item[2] == "green" else 50 if "red" else self._curr_map[item[0]+self._offset[0]][item[1]+self._offset[1]]
